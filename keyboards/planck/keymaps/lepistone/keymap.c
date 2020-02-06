8052fab9d8e802ed75f4386b9b051be8379ff098
Leonardo Pistone <leonardo@pistone.me>
2020-02-06 22:36:15 +0100

    Similar \ [ ] for Planck and Ergodox

--- keyboards/planck/keymaps/lepistone/keymap.c
+++ keyboards/planck/keymaps/lepistone/keymap.c
@@ -32,20 +32,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,---------------------------------------------------------------------------------------------.
- * | Tab      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp       |
+ * | Tab      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |     \      |
  * |----------+------+------+------+------+------+------+------+------+------+------+------------|
  * | Ctrl/Esc |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |     "      |
  * |----------+------+------+------+------+------+------+------+------+------+------+------------|
  * | Shift    |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift/Enter |
  * |----------+------+------+------+------+------+------+------+------+------+------+------------|
- * | `        | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right       |
+ * | `        | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left |   [  |   ]  |Right       |
  * `---------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-    KC_GRAVE,       KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_GRAVE,       KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_LBRC, KC_RBRC, KC_RGHT
 ),
 
 /* Lower
