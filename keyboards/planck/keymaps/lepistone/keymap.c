721da6f828146d3782e9dac548ea71ddc1b1d287
Leonardo Pistone <leonardo@pistone.me>
2020-02-08 12:57:30 +0100

    Double ' as Lower, symmetric to Ctrl

--- keyboards/planck/keymaps/lepistone/keymap.c
+++ keyboards/planck/keymaps/lepistone/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------------------------------------------------.
  * | Tab      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |     \      |
  * |----------+------+------+------+------+------+------+------+------+------+------+------------|
- * | Ctrl/Esc |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |     "      |
+ * | Ctrl/Esc |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Lower/'     |
  * |----------+------+------+------+------+------+------+------+------+------+------+------------|
  * | Shift    |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift/Enter |
  * |----------+------+------+------+------+------+------+------+------+------+------+------------|
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    LCTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    LCTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(LOWER, KC_QUOT),
     KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
     LT(LOWER, KC_GRV), KC_MINS, KC_EQL,  KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  XXXXXXX, KC_LALT, KC_LBRC, KC_RBRC, RAISE
 ),
