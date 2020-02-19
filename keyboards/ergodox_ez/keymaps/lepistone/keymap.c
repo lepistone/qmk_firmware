a87600877a2a8f70ae9e5213471f4e3a439d6ac1
Leonardo Pistone <leonardo@pistone.me>
2020-02-19 13:48:03 +0100

    Standard grave

--- keyboards/ergodox_ez/keymaps/lepistone/keymap.c
+++ keyboards/ergodox_ez/keymaps/lepistone/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,-------------------------------------------------------.
- * |   =     |   1  |   2  |   3  |   4  |   5  | LEFT |           | Mute |   6  |   7  |   8  |   9  |   0  |   -        |
+ * |   `     |   1  |   2  |   3  |   4  |   5  | LEFT |           | Mute |   6  |   7  |   8  |   9  |   0  |   -        |
  * |---------+------+------+------+------+-------------|           |------+------+------+------+------+------+------------|
  * | Tab     |   Q  |   W  |   E  |   R  |   T  |  L1  |           | Vol+ |   Y  |   U  |   I  |   O  |   P  |   \        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+------------|
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,
+  KC_GRV,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),
   LCTL_T(KC_ESC),  KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
   KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
